0dcf794defe1b6b645c62c3f41effb71fbf79f4c
Lester <LesterCuasay@live.co.uk>
2025-10-20 16:36:33 +0100

    style - adjusted padding

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -4,13 +4,10 @@ import Typewriter from "./Typewriter";
 
 const Sidebar = () => {
   return (
-    <div className="p-10">
+    <div className="">
       <h3>Hello, It's me</h3>
       <h1>Lester Cuasay</h1>
       <h3>And I'm a <span><Typewriter /></span></h3>
-      <p className="mt-2">
-        I'm a software engineer with a passion for building web applications.
-      </p>
     </div>
   );
 };
