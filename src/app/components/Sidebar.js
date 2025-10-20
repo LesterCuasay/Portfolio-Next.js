e9e6848dfd4f0b959ea90288018200ee674e96aa
Lester <LesterCuasay@live.co.uk>
2025-10-20 16:16:56 +0100

    feat - created typewriter component

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Typewriter from "./Typewriter";
+
 
 const Sidebar = () => {
   return (
     <div className="p-10">
-      <h1>Hi, My name is Lester Cuasay</h1>
+      <h3>Hello, It's me</h3>
+      <h1>Lester Cuasay</h1>
+      <h3>And I'm a <span><Typewriter /></span></h3>
       <p className="mt-2">
         I'm a software engineer with a passion for building web applications.
       </p>
---
+++ src/app/components/Typewriter.js
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Typewriter = () => {
+  const words = ["Web Developer", "Front-end Developer", "React Developer"];
+  const [text, setText] = useState("");
+  const [index, setIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [pause, setPause] = useState(false);
+
+  const typeSpeed = 150;
+  const deleteSpeed = 75;
+  const pauseDuration = 1000;
+
+  useEffect(() => {
+    const currentWord = words[index % words.length];
+
+    if (pause) return;
+
+    const speed = isDeleting ? deleteSpeed : typeSpeed;
+
+    const timeout = setTimeout(() => {
+      setText((prev) =>
+        isDeleting
+          ? currentWord.substring(0, prev.length - 1)
+          : currentWord.substring(0, prev.length + 1)
+      );
+
+      if (!isDeleting && text === currentWord) {
+        setPause(true);
+        setTimeout(() => setIsDeleting(true), pauseDuration);
+        setTimeout(() => setPause(false), pauseDuration);
+      } else if (isDeleting && text === "") {
+        setIsDeleting(false);
+        setIndex((prev) => prev + 1);
+      }
+    }, speed);
+
+    return () => clearTimeout(timeout);
+  }, [text, isDeleting, pause, index]);
+  return (
+    <span className="text-3xl">
+      {text}
+      <span className="animate-pulse">|</span>
+    </span>
+  );
+};
+
+export default Typewriter;
