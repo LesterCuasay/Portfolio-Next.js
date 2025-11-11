87efb8bf0f092ba5932e626d1c8bd3e319468c8c
Lester <LesterCuasay@live.co.uk>
2025-11-11 13:48:41 +0000

    feat - restructed sidebar

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Typewriter from "./Typewriter";
 
+
 const Sidebar = () => {
   return (
-    <div className="">
-      <h3 className="text-xl">Hello, It's me</h3>
-      <h1 className="text-3xl">Lester Cuasay</h1>
-      <h3 className="text-xl">And I'm a </h3>
-      <p className="text-2xl md:text-3xl">
-        <Typewriter />
-      </p>
-    </div>
+    <header className="flex justify-between flex-col h-full">
+      <div>
+        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+          Lester Cuasay
+        </h1>
+        <h2 className="mt-3 text-lg font-medium tracking-tight sm:text-xl">
+          Junior Front End Developer
+        </h2>
+        <p className="mt-4 max-w-xs leading-normal">
+          I build accessible, user-friendly web applications.
+        </p>
+      </div>
+
+     
+    </header>
   );
 };
 
