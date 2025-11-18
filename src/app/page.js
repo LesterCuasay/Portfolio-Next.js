c70b61e12ef0b7829b4f1131e20804a6dfa078b2
Lester <LesterCuasay@live.co.uk>
2025-11-18 15:35:09 +0000

    feat - added experience section into homepagE

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutSection from "./components/AboutSection";
+import ExperienceSection from "./components/ExperienceSection";
 
 const Home = () => {
   return (
     <div>
       <AboutSection />
+      <ExperienceSection />
     </div>
   );
 };
