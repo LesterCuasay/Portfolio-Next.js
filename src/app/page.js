a48c6d7e94c6113629292ee04e370d3b1de37db3
Lester <LesterCuasay@live.co.uk>
2025-11-26 14:52:05 +0000

    feat - added project section into homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "./components/AboutSection";
 import ExperienceSection from "./components/ExperienceSection";
+import ProjectSection from "./components/ProjectSection";
 
 const Home = () => {
   return (
     <div>
       <AboutSection />
       <ExperienceSection />
+      <ProjectSection />
     </div>
   );
 };
