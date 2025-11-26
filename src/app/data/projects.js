b7d0c0b4265300ebe5a67c3ba30c8cf2db2e30cf
Lester <LesterCuasay@live.co.uk>
2025-11-26 15:12:17 +0000

    feat - updated project description

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -2,14 +2,14 @@ export const projects = [
   {
     title: "Club Heaven",
     description:
-      "Developing and maintaining websites for various clients, focusing on responsive design and user experience.",
+      "Built a website for a company that manages nightclubs and event venues in Albufeira, Portugal. Focusing on responsive design and user experience. Actively maintaining and updating the site. Improving SEO through Google Search Console and Analytics.",
     technologies: ["Astro", "React", "TailwindCSS"],
-    link: "https://clubheaven.com/"
+    link: "https://clubheaven.com/",
   },
   {
     title: "Heaven Beach Club",
-    description: "Leading a team in a high-volume kitchen environment.",
+    description: "Second Iteration of website for the company in Portugal",
     technologies: ["Astro", "React", "TailwindCSS"],
-    link: "https://heavenbeachclub.com/"
+    link: "https://heavenbeachclub.com/",
   },
 ];
